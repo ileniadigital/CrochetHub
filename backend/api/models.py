b05d472fa763f8932eae095d92071e93977bcf09
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-10-28 17:14:12 +0000

    Added ProjectYarn and Project Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -23,3 +23,20 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
+
+# ProjectYarn Model to store yarn needed for a project
+class ProjectYarn(models.Model):
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+    yarn = models.ForeignKey(Yarn, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+# Project Model
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    yarn = models.ManyToManyField(Yarn, through=ProjectYarn)
+    pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_started = models.DateField()
+    date_finished = models.DateField()
+    notes = models.TextField()
