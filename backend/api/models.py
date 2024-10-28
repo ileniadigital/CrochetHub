729fbeae66decbb08d7a339f596fd1c8fdf6d639
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-10-28 17:03:33 +0000

    set yarn model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Yarn Model
+class Yarn(models.Model):
+    brand= models.CharField(max_length=255)
+    weight= models.IntegerField
+    colour = models.TextChoices("Colour", "RED ORANGE YELLOW GREEN BLUE PURPLE PINK BROWN BLACK WHITE GREY")
+    material = models.TextChoices("Material", "WOOL COTTON ACRYLIC POLYESTER")
+    price = models.FloatField()
+    yardage = models.IntegerField()
+    hook_size = models.FloatField()
+    available= models.BooleanField()
+
+# Tutorials Model
+
+# User Model
