626119e544da4891ea0b1e2c349fa0c0b32401d4
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-10-29 12:32:33 +0000

    changed my models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,29 +1,31 @@
 from django.contrib import admin
-from .models import Yarn, Pattern, User, ProjectYarn, Project
+from .models import Yarn, Pattern, User, PatternYarn, Project
 
 @admin.register(Yarn)
 class YarnAdmin(admin.ModelAdmin):
-    list_display = ('brand', 'weight', 'colour', 'material', 'price', 'yardage', 'hook_size', 'available')
-    list_filter = ('colour', 'material', 'available')
+    list_display = ('brand', 'weight', 'colour', 'material', 'price', 'yardage', 'hook_size')
     search_fields = ('brand', 'colour', 'material')
 
 @admin.register(Pattern)
 class PatternAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'published', 'link', 'transcript')
-    search_fields = ('title', 'description')
+    list_display = ('title', 'published', 'link', 'transcript')
+    search_fields = ('title',)
+    list_filter = ('published',)
+    date_hierarchy = 'published'
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email')
     search_fields = ('username', 'email')
 
-@admin.register(ProjectYarn)
-class ProjectYarnAdmin(admin.ModelAdmin):
-    list_display = ('project', 'yarn', 'quantity')
-    search_fields = ('project__title', 'yarn__brand')
+@admin.register(PatternYarn)
+class PatternYarnAdmin(admin.ModelAdmin):
+    list_display = ('pattern', 'yarn', 'quantity')
+    search_fields = ('pattern__title', 'yarn__brand')
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description','user', 'date_started', 'date_finished', 'notes')
-    list_filter = ('date_started', 'date_finished')
-    search_fields = ('title', 'description', 'user__username')
+    list_display = ('title', 'pattern', 'user', 'date_started', 'finished', 'date_finished')
+    search_fields = ('title', 'pattern__title', 'user__username')
+    list_filter = ('finished', 'date_started', 'date_finished')
+    date_hierarchy = 'date_started'
--- backend/api/models.py
+++ backend/api/models.py
@@ -23,24 +23,23 @@ class Material(models.TextChoices):
 
 class Yarn(models.Model):
     '''Yarn Model to store yarn information'''
-    brand= models.CharField(max_length=255)
-    weight= models.IntegerField()
+    brand = models.CharField(max_length=255)
+    weight = models.IntegerField()
     colour = models.CharField(max_length=50, choices=Colour.choices)
     material = models.CharField(max_length=50, choices=Material.choices)
     price = models.FloatField()
     yardage = models.IntegerField()
     hook_size = models.FloatField()
-    available= models.BooleanField()
-
 
 class Pattern(models.Model):
     '''Pattern Model to store pattern information'''
+    id= models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
     published = models.DateField()
     link = models.URLField()
     transcript = models.TextField()
-
+    yarns = models.ManyToManyField(Yarn, through='PatternYarn')
 
 class User(models.Model):
     '''User Model to store user information'''
@@ -48,21 +47,19 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=255)
 
-
-class ProjectYarn(models.Model):
-    '''ProjectYarn Model to store yarn needed for a project'''
-    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+class PatternYarn(models.Model):
+    '''PatternYarn Model to store yarn needed for a pattern'''
+    pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
     yarn = models.ForeignKey(Yarn, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-
 
 class Project(models.Model):
     '''Project Model to store project with materials and pattern'''
     title = models.CharField(max_length=255)
     description = models.TextField()
-    yarn = models.ManyToManyField(Yarn, through=ProjectYarn)
     pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_started = models.DateField()
+    finished = models.BooleanField(default=False)
     date_finished = models.DateField()
     notes = models.TextField()
