2acb27943a61c78fbd7712c6303648724d3afe24
Ilenia Maietta <i.maietta@se22.qmul.ac.uk>
2024-11-04 15:32:58 +0000

    accidentally broke post for all mdoels except patternyarn, fixed by adding / at the end of url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -22,16 +22,16 @@ from .views import *
 
 urlpatterns = [
     # API entry points should be defined here
-    path('yarn', yarn_api_view, name='yarn'),
+    path('yarn/', yarn_api_view, name='yarn'),
     path('yarn/<int:id>/update/', yarn_put, name='yarnUpdate'),
     path('yarn/<int:id>/delete/', yarn_delete, name='yarnDelete'),
-    path('pattern', pattern_api_view, name='pattern'),
+    path('pattern/', pattern_api_view, name='pattern'),
     path('pattern/<int:id>/update/', pattern_put, name='patternUpdate'),
     path('pattern/<int:id>/delete/', pattern_delete, name='patternDelete'),
-    path('user', user_api_view, name='user'),
+    path('user/', user_api_view, name='user'),
     path('user/<int:id>/update/', user_put, name='userUpdate'),
     path('user/<int:id>/delete/', user_delete, name='userDelete'),
-    path('project', project_api_view, name='project'),
+    path('project/', project_api_view, name='project'),
     path('project/<int:id>/update/', project_put, name='projectUpdate'),
     path('project/<int:id>/delete/', project_delete, name='projectDelete'),
     path('patternyarn/', patternyarn_api_view, name='patternyarn'),
